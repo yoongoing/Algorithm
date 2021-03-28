4752b98548cfdba7eb0cc19add1f8442b55d511b
윤가영 <yun_going@naver.com>
2021-03-28 16:10:59 +0900

    20210328

--- DFS,BFS/2098.cpp
+++ DFS,BFS/2098.cpp
@@ -1,106 +1,64 @@
 #include <iostream>
-#include <vector>
-#include <cstring>
+#include <algorithm>
+#include <cstring> //memset
 
 using namespace std;
 
+const int MAX = 16;
+const int INF = 987654321;
+
 int N;
-vector<int> v[11];
-int civil[11];
-int devide[11];
-int visited[11] = {0,};
-int ans = 987654321;
-
-int search(int idx, int type, int cost){
-    int cnt = 1;
-    visited[idx] = 1;
-
-    for(int j=0; j<v[idx].size(); j++){
-        
-        if(type==devide[v[idx][j]] && !visited[v[idx][j]]){
-            cnt += search(v[idx][j], type, cost+civil[v[idx][j]]);
-        }
-    }
-    return cnt;
-}
-
-int cal(int cnt){
-    int sec0 = N - cnt;
-    int sec1 = cnt;
-
-    int idx0, sum0 = 0;
-    int idx1, sum1 = 0;
-    for(int i=1; i<=N; i++){
-        if(devide[i]){ // 1구역이라면
-            idx1 = i;
-            sum1 += civil[i];
-        }    
-        else{ // 0구역이라면
-            idx0 = i;
-            sum0 += civil[i];
-        } 
-    }
-    // cout<<sum1<<" "<<sum0<<"\n";
-    memset(visited, 0, sizeof(visited));
-    int cnt0 = search(idx0, 0, 0);
-    memset(visited, 0, sizeof(visited));
-    int cnt1 = search(idx1, 1, 0);
-
-    if(cnt0!=sec0 || cnt1!=sec1)
-        return -1;
-
-    return abs(sum0 - sum1);
-}
+int W[MAX][MAX];
+int cache[MAX][1 << MAX];
 
 
-void dfs(int idx){
-    if(idx == N+1){
-        int cnt = 0;
-        for(int i=1; i<=N; i++)
-            if(devide[i])
-                cnt++;
-        if(cnt==0 || cnt==N)
-            return; // 하나로만 고르는 경우는 제외
-        
-        int diff = cal(cnt);
-        // cout<<diff<<"\n";
-        if(diff != -1)
-            ans = min(ans, diff);
-        
-        return;
+int TSP(int here, int visited){
+    //기저 사례: 모든 도시를 다 방문했을 때는 0번 도시로 돌아가고 종료
+    if (visited == (1 << N) - 1){
+        if (W[here][0] != 0)
+            return W[here][0];
+        return INF;
     }
 
-    devide[idx] = 1;
-    dfs(idx + 1);
+    //메모이제이션
+    int &result = cache[here][visited];
+    if (result != -1)
+        return result;
+    result = INF;
 
-    devide[idx] = 0;
-    dfs(idx + 1);
+    //다음 방문할 도시를 전부 시도
+
+    for (int next = 0; next < N; next++){
+
+        //이미 방문한 도시
+        if (visited & (1 << next) || W[here][next]==0)
+            continue;
+
+        int candidate = W[here][next] + TSP(next, visited + (1 << next));
+        result = min(result, candidate);
+    }
+
+    return result;
 }
 
+ 
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
     cin>>N;
-    for(int i=1; i<=N; i++)
-        cin>>civil[i];
-    
-    for(int i=1; i<=N; i++){
-        int n;
-        cin>>n;
-        for(int j=0; j<n; j++){
-            int node;
-            cin>>node;
-            v[i].push_back(node);
-        }
-    }
+    if (N<2 || N>MAX)
+        exit(-1);
 
-    dfs(1);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> W[i][j];
 
-    if(ans == 987654321)
-        cout<<"-1\n";
-    else
-        cout<<ans<<"\n";
+    int result = INF;
+    memset(cache, -1, sizeof(cache));
+
+    cout << TSP(0, 1) << endl;
     return 0;
 }
+
