cd7327fb48669b2f5e6dae22cb782843f62320a9
윤가영 <yun_going@naver.com>
2021-07-10 01:17:59 +0900

    20210710

--- BOJ/Two Pointer/10713.cpp
+++ BOJ/Two Pointer/10713.cpp
@@ -1,10 +1,38 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    
+
+    int N,M;
+    cin>>N>>M;
+
+    long long int P[1001];
+    long long int sum[1001] = {0,};
+    for(int i=0; i<M; i++)
+        cin>>P[i];
+    for(int i=0; i<M-1; i++){
+        int a = min(P[i], P[i+1]);
+        int b = max(P[i], P[i+1]);
+        sum[a]++;
+        sum[b]--;
+    }
+
+    for(int i=1; i<=N; i++)
+        sum[i] += sum[i-1];
+
+    long long int ans = 0;
+    for(int i=1; i<N; i++){
+        int a,b,c;
+        cin>>a>>b>>c;
+        
+        if(sum[i] != 0)
+            ans += min(a*sum[i], b*sum[i]+c);
+    }
+    cout<<ans<<"\n";
+
     return 0;
 }
